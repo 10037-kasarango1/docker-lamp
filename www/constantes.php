971bd7336ed8d34d048488cf9418d115c6f53eaa
Katherine Sarango <107556558+10037-kasarango1@users.noreply.github.com>
2024-08-07 11:49:17 -0500

    V1

---
+++ www/constantes.php
@@ -0,0 +1,6 @@
+<?php
+  define("SERVER", "db"); // Cambiar de "localhost" a "db" para que se conecte al contenedor db
+  define("USER", "root");
+  define("PASS", "123");
+  define("BD", "matriculacionfinal");
+?>
